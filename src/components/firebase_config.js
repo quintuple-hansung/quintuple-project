a7b65035ac007b56aac11788751444793d55a9b4
sysliar <sysliar@gmail.com>
2022-10-26 11:20:15 +0900

    use dotenv

--- src/components/firebase_config.js
+++ src/components/firebase_config.js
@@ -1,11 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
+import firebase from 'firebase';
+import dotenv from 'dotenv';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+dotenv.config();
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -15,6 +18,7 @@ const firebaseConfig = {
 	appId: process.env.REACT_APP_FIREBASE_APPID,
 	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
 };
+export default firebaseConfig;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
