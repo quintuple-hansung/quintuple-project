fa7aeab5c968281c6753b706ec3813ec5d0e3208
sysliar <sysliar@gmail.com>
2022-11-01 00:52:39 +0900

    refactor: edit routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { Login, Index, Home } from './components/Login';
+import Index from './pages/Index';
+import Login from './pages/Login';
+import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Introduce1 from './pages/Index';
 
 function App() {
 	return (
 		<div>
 			<Router>
 				<Routes>
-					<Route path="/" element={<Index />} />
+					<Route path="/" element={<Introduce1 />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/home" element={<Home />} />
 				</Routes>
