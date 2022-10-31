7cdf9a5254ef4f274e19b888bd4cd8260cbfe035
sysliar <sysliar@gmail.com>
2022-11-01 00:35:13 +0900

    feat: add Home.js, route /home

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './components/Login';
-import Index from './components/Index';
+import { Login, Index, Home } from './components/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import app from './components/firebase_config';
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Index/>}/>
-          <Route path="/login" element={<Login/>}/>
-        </Routes>
-      </Router>
-    </div>
-  );
+	return (
+		<div>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Index />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/home" element={<Home />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
