a2a3a9cc528463ffc80d339501e0826aa4cbbeb9
sysliar <sysliar@gmail.com>
2022-11-01 18:24:48 +0900

    refactor: remove body tags

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import './App.css';
 import Login from './pages/Login';
 import Intro from './pages/Intro';
+import Main from './pages/Main';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import app from './pages/firebase_config';
 import Join from './pages/Join';
 
-
 function App() {
-  return (
-    <body>
-        <div className='container'>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Intro/>}/>
-            {/*<Route path="/" element={<Index/>}/>*/}
-            <Route path="/login" element={<Login/>}/>
-            <Route path='/join' element={<Join/>}/>
-            {/* Route path="/main", element={Main/>}/>*/}
-          </Routes>
-        </Router>
-    </div>
-    </body>
-  );
+	return (
+		<div className="container">
+			<Router>
+				<Routes>
+					<Route path="/" element={<Intro />} />
+					{/*<Route path="/" element={<Index/>}/>*/}
+					<Route path="/login" element={<Login />} />
+					<Route path="/join" element={<Join />} />
+					<Route path="/main" element={<Main />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
