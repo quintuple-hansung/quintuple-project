a419c0e0c046018c93a8c5ac937d39e19623a02c
sysliar <sysliar@gmail.com>
2022-11-02 23:10:07 +0900

    feat: main page ui

--- src/components/main/CardThumbnail.js
+++ src/components/main/CardThumbnail.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import DownloadImage from '../DownloadImage';
+
 function CardThumbnail() {
 	return;
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Card from '../components/main/Card';
+import '../styles/Main.css';
 function Main() {
 	return <Card />;
 }
