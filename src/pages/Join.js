528d8c804464db183e823af811463155ab5d74c5
jueun0725 <kimjueun010@naver.com>
2022-11-06 00:58:30 +0900

    회원가입 firebase

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -2,26 +2,40 @@ import React, { useState, useEffect } from 'react';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
 import '../styles/Join.css';
+import {firestore} from "../components/firebase_config";
+import {addDoc, collection} from 'firebase/firestore';
 
 function Join() {
     const [loginEmail, setLoginEmail] = useState("");
     const [loginPw, setLoginPw] = useState("");
+    const [userName, setUserName] = useState("");
 
     const auth = getAuth();
     const navigate = useNavigate();
     
+     //firestore의 user 컬렉션을 가져옴
+     const usersCollectionRef = collection(firestore, "user");
+    
+     const signup = async () => {
+         await addDoc(usersCollectionRef, {name:userName, email:loginEmail, password:loginPw});
+     }
+ 
+    /*
     const signup = async() => {
         console.log('Join button pressed');
         const result = await createUserWithEmailAndPassword(auth, loginEmail, loginPw);
         console.log(result);
         navigate('/login');
     }
+    */
     
     return (
         <div className='join_form'>
+            <p><input type="text" placeholder="Name" onChange={(e) => { setUserName(e.target.value); }}/></p>
             <p><input type="text" placeholder="Email" onChange={(e) => { setLoginEmail(e.target.value); }}/></p>
             <p><input type="password"placeholder="EmailPassword" onChange={(e) => { setLoginPw(e.target.value); }}/></p>
-            <button onClick={signup}>CreateUser</button>
+            <button onClick={signup}>회원가입</button><br/>
+            <button onClick={() => (window.location.href = '/login')}>로그인 페이지로</button>
         </div>
     )
 
