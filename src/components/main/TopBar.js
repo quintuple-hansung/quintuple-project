1d78239ef98e76eee6fb06a0d01f72e44b748fe1
sysliar <sysliar@gmail.com>
2022-11-17 14:33:24 +0900

    feat: add top bar

--- src/components/main/TopBar.js
+++ src/components/main/TopBar.js
@@ -1,38 +1,41 @@
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
 import React from 'react';
 import PropTypes from 'prop-types';
+import HomeIcon from '@mui/icons-material/Home';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import SearchAppBar from './SearchBar';
+import { Link } from 'react-router-dom';
 
 export default function TopBar() {
-	const [value, setValue] = React.useState(0);
-
-	const handleChange = (event, newValue) => {
-		setValue(newValue);
-	};
-
 	return (
-		<Box sx={{ width: '100%' }}>
-			<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-				<Tabs
-					value={value}
-					onChange={handleChange}
-					aria-label="basic tabs example">
-					<Tab label="Item One" {...a11yProps(0)} />
-					<Tab label="Item Two" {...a11yProps(1)} />
-					<Tab label="Item Three" {...a11yProps(2)} />
-				</Tabs>
-			</Box>
-			<TabPanel value={value} index={0}>
-				Item One
-			</TabPanel>
-			<TabPanel value={value} index={1}>
-				Item Two
-			</TabPanel>
-			<TabPanel value={value} index={2}>
-				Item Three
-			</TabPanel>
+		<Box sx={{ flexGrow: 2 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<Link to="/main" style={{ textDecoration: 'none', color: 'inherit' }}>
+						<IconButton
+							edge="start"
+							color="inherit"
+							sx={{ width: 50, height: 50, marginInlineEnd: 5 }}>
+							<HomeIcon />
+						</IconButton>
+					</Link>
+					<Typography variant="h5" component="div" sx={{ flexGrow: 3 }}>
+						quintuple
+					</Typography>
+					<SearchAppBar />
+					<Link
+						to="/mypage"
+						style={{ textDecoration: 'none', color: 'inherit' }}>
+						<Button color="inherit" sx={{ width: 100 }}>
+							My Page
+						</Button>
+					</Link>
+				</Toolbar>
+			</AppBar>
 		</Box>
 	);
 }
