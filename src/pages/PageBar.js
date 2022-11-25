a859523fe2c47d36a217ccb7dd4e2f427f5df384
jueun0725 <kimjueun010@naver.com>
2022-11-26 01:08:45 +0900

    포트폴리오 작성페이지

---
+++ src/pages/PageBar.js
@@ -0,0 +1,69 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import React from 'react';
+import PropTypes from 'prop-types';
+import HomeIcon from '@mui/icons-material/Home';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+import { Link } from 'react-router-dom';
+
+//포트폴리오 작성페이지 bar
+export default function PageBar() {
+	return (
+		<Box sx={{ flexGrow: 2 }}>
+			<AppBar position="static">
+				<Toolbar  sx={{ justifyContent: 'space-between' }} style={{background : '#2E3B55'}}>
+					<Link to="/main" style={{ textDecoration: 'none', color: 'inherit'}}>
+						<IconButton
+							edge="start"
+							color="inherit"
+							sx={{ width: 50, height: 50, marginInlineEnd: 5 }}>
+							<HomeIcon />
+						</IconButton>
+					</Link>
+					<Typography variant="h5" component="div" textAlign="center" sx={{ flexGrow: 3 }}  fontFamily= "esamanru Light">
+						QUINTUPLE
+					</Typography>
+		
+					<Link
+						to="/mypage"
+						style={{ textDecoration: 'none', color: 'inherit' }}
+                        fontFamily= "esamanru Light">
+						<Button color="inherit" sx={{ width: 100 }} >
+							My Page
+						</Button>
+					</Link>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
+}
+
+function TabPanel(props) {
+	const { children, value, index, ...other } = props;
+
+	return (
+		<div
+			role="tabpanel"
+			hidden={value !== index}
+			id={`simple-tabpanel-${index}`}
+			aria-labelledby={`simple-tab-${index}`}
+			{...other}>
+			{value === index && (
+				<Box sx={{ p: 3 }}>
+					<Typography>{children}</Typography>
+				</Box>
+			)}
+		</div>
+	);
+}
+
+TabPanel.propTypes = {
+	children: PropTypes.node,
+	index: PropTypes.number.isRequired,
+	value: PropTypes.number.isRequired,
+};
+
--- src/pages/PortFolio.js
+++ src/pages/PortFolio.js
@@ -7,6 +7,8 @@ import {firestore} from "../components/firebase_config";
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { doc, getDoc, updateDoc } from 'firebase/firestore/lite';
 import { useNavigate } from 'react-router-dom';
+import PageBar from './PageBar.js';
+import '../styles/Portfolio.css';
 
 function Portfolio(){
   //const [value, setValue] = React.useState('Controlled');
@@ -94,6 +96,9 @@ function Portfolio(){
 
   return (
     <>
+    <PageBar />
+    <section className="content">
+        <p>포트폴리오에 들어갈 내용을 작성하세요. </p>
         <Box
           component="form"
           sx={{
@@ -102,28 +107,26 @@ function Portfolio(){
           noValidate
           autoComplete="off"
         >
-          <div>
-            <div>
+          <div className="content1">
+              <div class='item'>
               <TextField
                 id="outlined-multiline-static"
                 label="이름"
-                multiline
                 rows={1}
-                defaultValue=""
+                defaultValue=''
                 onChange={handleName}
               />
-            </div>
-            <div>
-              <TextField
+              </div>
+              <div>
+              <TextField 
                 id="outlined-multiline-static"
                 label="나이"
-                multiline
                 rows={1}
                 defaultValue=""
                 onChange={handleAge}
               />
-            </div>
-            <div>
+              </div>
+              <div>
               <TextField
                 id="outlined-multiline-static"
                 label="학력"
@@ -131,18 +134,69 @@ function Portfolio(){
                 rows={1}
                 defaultValue=""
                 onChange={handleEducation}
+            
               />
-            </div>
-          </div>
-          <div>
+           </div>
+           <div>
             <TextField
               id="outlined-multiline-static"
               label="자기소개"
               multiline
-              rows={10}
+              rows={3}
               defaultValue=""
               onChange={handleIntroduce}
             />
+            </div>
+            <div>
+            <TextField
+              id="outlined-multiline-static"
+              label="프로젝트 제목"
+              multiline
+              rows={10}
+              defaultValue=""
+              onChange={handleTitle}
+            />
+          </div>
+          <div>
+            <TextField
+              id="outlined-multiline-static"
+              label="프로젝트 설명"
+              multiline
+              rows={10}
+              defaultValue=""
+              onChange={handleDesription}
+            />
+           </div>
+           <div>
+            <TextField
+              id="outlined-multiline-static"
+              label="사용했던 언어"
+              multiline
+              rows={10}
+              defaultValue=""
+              onChange={handleLanguage}
+            />
+            </div>
+            <div>
+            <TextField
+              id="outlined-multiline-static"
+              label="사용한 기술"
+              multiline
+              rows={5}
+              defaultValue=""
+              onChange={handleStack}
+            />
+            </div>
+            <div>
+            <TextField
+              id="outlined-multiline-static"
+              label="자신의 주요 언어"
+              multiline
+              rows={5}
+              defaultValue=""
+              onChange={handleTLanguage}
+            />
+            </div>
           </div>
         </Box>
         <div>
@@ -150,6 +204,8 @@ function Portfolio(){
             <Button variant="outlined" onClick={submitInfo}>다음</Button>
           </Stack>
         </div>
+
+      </section>
       </>
   )
 }
