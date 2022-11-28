6f9e091ae48f0c5881c6928ec838450ddf68a3fd
jueun0725 <kimjueun010@naver.com>
2022-11-28 13:12:40 +0900

    pdf 추출 버튼

--- src/pages/temp1/Template1.js
+++ src/pages/temp1/Template1.js
@@ -2,11 +2,65 @@ import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
 import { chosenTheme } from "../theme";
 import { GlobalStyles } from "../global";
+import html2canvas from 'html2canvas'; // javascript 페이지 스크린샷 라이브러리
+import jsPDF from 'jspdf'; // JavaScript에서 PDF를 생성하는 라이브러리.
+import Button from '@mui/material/Button'; // Mui Button 사용
+import SaveIcon from '@mui/icons-material/Save'; // Mui 저장 아이콘 사용
 
 function Template1(){
+  //PDF로 추출
+	const exportPDF = () => {
+		// PDF가 캡쳐해서 변환한 이미지가 클 경우 잘려서 출력되는 것 해결해야 함!
+		// => 참조 사이트 https://jeffrey-oh.tistory.com/363 
+		// 현재 페이지에서 HTML element를 가져오는 것이 아닌 다른 페이지의 componenet 가져오는 것 해결 해야 함!
+		const element = document.querySelector("div.mypage_form_captureTarget") // 캡쳐할 HTML element
+
+		html2canvas(element)
+			.then(function (canvas) {
+
+				var capturedImgData = canvas.toDataURL(); // canvas로 캡쳐한 data를 base64 Data로 변환(jsPDF에서 사용)
+
+				// 새 PDF 생성
+				var doc = new jsPDF('p', "pt", "a4");
+				var position = 0;
+
+
+				var imgWidth = 595; // 이미지 가로 길이() A4 기준
+				var pageHeight = 842; // 이미지 세로 길이() A4 기준 / A4 기준 (595,842)pt
+				var imgHeight = canvas.height * imgWidth / canvas.width; // 이미지 세로 길이를 A4기준에 맞게 변환
+				var heightLeft = imgHeight;
+				var margin = 0;
+
+				console.log(`canvas.width is ${canvas.width} canvas.height is ${canvas.height}`) // canvas 크기(captured img) 출력
+				console.log(`imgWidth is ${imgWidth} imgHeight.width is ${imgHeight}`) // pdf 에 출력될 변환된 capture img 크기 출력
+				console.log(`margin is ${margin} postion is ${position}`) // 출력되는 시작지점(x,y) 출력
+				console.log(`HTML element width is ${element.offsetWidth} HTML element height is ${element.offsetHeight}`) // targetHTMLelemnt의 원래 크기 출력
+
+				// PDF에 캡쳐한 img 붙이기
+				// 첫 페이지 출력
+				doc.addImage(capturedImgData, 'PNG', margin, position, imgWidth, imgHeight);
+				heightLeft -= pageHeight;
+
+				// 한 페이지 이상일 경우 루프 돌면서 출력
+				while (heightLeft >= 842) {
+					position = heightLeft - imgHeight;
+					doc.addPage();
+					doc.addImage(capturedImgData, 'jpeg', margin, position, imgWidth, imgHeight);
+					heightLeft -= pageHeight;
+				}
+
+				// 파일 저장
+				doc.save('MyPortfolio.pdf');
+
+			})
+
+
+	};
+
 return (
-    <div className="mypage_form">
-      <div>
+  <>
+    <div className='mypage_form_captureTarget'>
+      
         <ThemeProvider theme={chosenTheme}>
           <>
             <GlobalStyles />
@@ -15,8 +69,14 @@ return (
             </div>
           </>
       </ThemeProvider>
+     
+
+      <div className="mypage_form_myPortfolioExportForm" style={{border:'5px solid rgba(46,59,85)'}}>
+					<Button sx={{ width: "500px", marginTop: "50px", marginBottom: "50px",bgcolor: '#2e3b55',fontFamily:'nanum', "&:hover": {backgroundColor:'#E8474C', cursor: "pointer"} }} onClick={() => exportPDF()} variant="contained" endIcon={<SaveIcon />}>내 포트폴리오 PDF로 저장하기</Button>
       </div>
+
     </div>
+    </>
   );
 }
 export default Template1;
--- src/pages/temp2/Template2.js
+++ src/pages/temp2/Template2.js
@@ -9,11 +9,10 @@ import { collection, doc, getDoc } from "firebase/firestore/lite";
 import { getAuth } from "firebase/auth";
 import { firestore } from "../../components/firebase_config";
 import self from "./img/self.png"
-import mock1 from "./img/mock1.png"
-import mock2 from "./img/mock2.png"
-import mock3 from "./img/mock3.png"
-import mock4 from "./img/mock4.png"
-import mock5 from "./img/mock5.png"
+import html2canvas from 'html2canvas'; // javascript 페이지 스크린샷 라이브러리
+import jsPDF from 'jspdf'; // JavaScript에서 PDF를 생성하는 라이브러리.
+import Button from '@mui/material/Button'; // Mui Button 사용
+import SaveIcon from '@mui/icons-material/Save'; // Mui 저장 아이콘 사용
 
 function EmojiBullet(props) {
    const {emoji, text} = props;
@@ -193,9 +192,58 @@ export default function Home() {
        </>;
    }
 
+   //PDF로 추출
+	const exportPDF = () => {
+		// PDF가 캡쳐해서 변환한 이미지가 클 경우 잘려서 출력되는 것 해결해야 함!
+		// => 참조 사이트 https://jeffrey-oh.tistory.com/363 
+		// 현재 페이지에서 HTML element를 가져오는 것이 아닌 다른 페이지의 componenet 가져오는 것 해결 해야 함!
+		const element = document.querySelector("div.mypage_form_captureTarget") // 캡쳐할 HTML element
+
+		html2canvas(element)
+			.then(function (canvas) {
+
+				var capturedImgData = canvas.toDataURL(); // canvas로 캡쳐한 data를 base64 Data로 변환(jsPDF에서 사용)
+
+				// 새 PDF 생성
+				var doc = new jsPDF('p', "pt", "a4");
+				var position = 0;
+
+
+				var imgWidth = 595; // 이미지 가로 길이() A4 기준
+				var pageHeight = 842; // 이미지 세로 길이() A4 기준 / A4 기준 (595,842)pt
+				var imgHeight = canvas.height * imgWidth / canvas.width; // 이미지 세로 길이를 A4기준에 맞게 변환
+				var heightLeft = imgHeight;
+				var margin = 0;
+
+				console.log(`canvas.width is ${canvas.width} canvas.height is ${canvas.height}`) // canvas 크기(captured img) 출력
+				console.log(`imgWidth is ${imgWidth} imgHeight.width is ${imgHeight}`) // pdf 에 출력될 변환된 capture img 크기 출력
+				console.log(`margin is ${margin} postion is ${position}`) // 출력되는 시작지점(x,y) 출력
+				console.log(`HTML element width is ${element.offsetWidth} HTML element height is ${element.offsetHeight}`) // targetHTMLelemnt의 원래 크기 출력
+
+				// PDF에 캡쳐한 img 붙이기
+				// 첫 페이지 출력
+				doc.addImage(capturedImgData, 'PNG', margin, position, imgWidth, imgHeight);
+				heightLeft -= pageHeight;
+
+				// 한 페이지 이상일 경우 루프 돌면서 출력
+				while (heightLeft >= 842) {
+					position = heightLeft - imgHeight;
+					doc.addPage();
+					doc.addImage(capturedImgData, 'jpeg', margin, position, imgWidth, imgHeight);
+					heightLeft -= pageHeight;
+				}
+
+				// 파일 저장
+				doc.save('MyPortfolio.pdf');
+
+			})
+
+
+	};
 
    return(
-      <>
+        <>
+      <div className='mypage_form_captureTarget'>
       <Box component={'main'} display={'flex'} flexDirection={{xs: 'column', md: 'row'}} alignItems={'center'}
            justifyContent={'center'} minHeight={'calc(100vh - 175px)'}>
          <Box className={classNames(Style.avatar, Style.shadowed)} alt={'image of developer'} style={{background: info.gradient}} component={'img'} src={me} width={{xs: '35vh', md: '40vh'}}
@@ -220,18 +268,15 @@ export default function Home() {
             <Terminal text={stacksText()}/>
             <Terminal text={skillsText()}/>
         </Box>    
+        </div>
+
+        <div className="mypage_form_myPortfolioExportForm" style={{border:'5px solid rgba(46,59,85)'}}>
+					<Button sx={{ width: "500px", marginTop: "50px", marginBottom: "50px",bgcolor: '#2e3b55',fontFamily:'nanum', "&:hover": {backgroundColor:'#E8474C', cursor: "pointer"} }} onClick={() => exportPDF()} variant="contained" endIcon={<SaveIcon />}>내 포트폴리오 PDF로 저장하기</Button>
+        </div>
 
 
-        {/*<Box>
-            <Grid container display={'flex'} justifyContent={'center'}>
-                {info.portfolio.map((project, index) => (
-                   <Grid item xs={12} md={6} key={index}>
-                       <PortfolioBlock image={project.image} live={project.live} source={project.source} title={project.title} />
-                   </Grid>
-                ))}
-            </Grid>
-                </Box>*/}
 
+      
       </>
 
    )
